0a158a023ef01cc8e7dfc09b865c82228184441f
Nicole Valentini <ncvalentini@gmail.com>
2022-04-20 18:57:29 -0600

    Add additional component files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useState, useEffect } from 'react'
-import getData from '../apiCalls'
+import getData from '../../apiCalls'
 
 function App() {
   const [drivers, setDrivers] = useState([])
