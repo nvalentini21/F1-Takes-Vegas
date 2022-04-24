d6b65ca6a856f5e1279d1c3c90f665c629e6628a
Nicole Valentini <ncvalentini@gmail.com>
2022-04-23 21:15:49 -0600

    Add filter to fun page

--- src/components/AllConstructorsPage/AllConstructors.js
+++
@@ -1,4 +0,0 @@
-const constructors = () => {}
-//put the api call for constructors here, since they will not
-//be displayed on the home page, and app doesn't need to know about
-//this info. Only the constructors page needs to know about the data. 
